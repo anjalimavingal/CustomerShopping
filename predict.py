86e61c4bee02e1a686c4ab632e0bb618cd4fb184
Anjali.Mavingal <anjali.mavingal@IN-HYD-P0194781.local>
2026-03-13 14:04:25 +0530

    changes updated

--- predict.py
+++ predict.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 model = joblib.load("model/model.pkl")
 def predict(features):
-    print("features", features)
     features = np.array(features, dtype=object).reshape(1, -1)
     columns = pd.read_csv('data/online vs store shopping dataset.csv').columns.tolist()[:-1]
     df = pd.DataFrame(features, columns=columns)
